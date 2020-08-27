791db465e46b43ff87b1c30472bcc4e986724ede
T3NSH11 <m.zaid@saedubai.com>
2020-08-27 12:34:24 +0400

    added health system

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -20,11 +20,12 @@ public class Tank : MonoBehaviour
     public GameObject FiringEnd;
 
     public int ID;
-
     private float Moving;
     public float Rotate;
+    public float collisiondelay;
     public int MoveSpeed;
     public int Recoil;
+    public int health;
     public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
@@ -35,8 +36,9 @@ public class Tank : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        collisiondelay += Time.deltaTime;
 
-        if (TurnBaseManager.PlayerTurn == ID)
+        if (TurnBaseManager.PlayerTurn == ID && health != 0)
         {
             Moving = Input.GetAxis("Vertical") * MoveSpeed;
             Rotate = Input.GetAxis("Horizontal") * 5;
@@ -55,7 +57,7 @@ public class Tank : MonoBehaviour
         else if(TurnBaseManager.PlayerTurn >= 2)
         {
             TurnBaseManager.PlayerTurn = 0;
-        }
+        }        
         
     }
 
@@ -65,6 +67,15 @@ public class Tank : MonoBehaviour
         {
             GetComponent<Rigidbody>().velocity = FiringEnd.transform.right * Moving;
             this.transform.Rotate(transform.up * Rotate);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collisiondelay > 0.3 && (collision.gameObject.tag == "Bullet_2" | collision.gameObject.tag == "Bullet_1"))
+        {
+            collisiondelay = 0;
+            health = health - 1;
         }
     }
 }
