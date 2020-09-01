f12b0ac12422d06421a74adf1b1d86be7f5565fb
T3NSH11 <m.zaid@saedubai.com>
2020-09-01 13:44:47 +0400

    fixed scene changes and coded healthbars

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -2,39 +2,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Tank : MonoBehaviour
 {
 
-    //public static bool playershot = false;
-    //private static bool player1turn;
-    //private static bool player2turn;
-    //public static string player1check;
-    //public static string player2check;
-
-    //public int id;
-    //public TurnBaseManager turnmanager;
     public Camera MainCam;
     public GameObject bullet;
     public GameObject FiringEnd;
+    public GameObject healthobject;
 
     public int ID;
     private float Moving;
     public float Rotate;
     public float collisiondelay;
+    public int damage;
     public int MoveSpeed;
     public int Recoil;
     public int health;
     public Rigidbody rb;
-    // Start is called before the first frame update
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         collisiondelay += Time.deltaTime;
@@ -61,12 +55,14 @@ public class Tank : MonoBehaviour
 
         if (ID == 0 && health == 0)
         {
-            SceneManager.LoadScene("player2wins", LoadSceneMode.Additive);
+            SceneManager.LoadScene("player2wins", LoadSceneMode.Single);
         }
         if (ID == 1 && health == 0)
         {
-            SceneManager.LoadScene("player1wins", LoadSceneMode.Additive);
+            SceneManager.LoadScene("player1wins", LoadSceneMode.Single);
         }
+
+        healthobject.GetComponent<Slider>().value = health;
     }
 
     private void FixedUpdate()
@@ -83,7 +79,7 @@ public class Tank : MonoBehaviour
         if (collisiondelay > 0.3 && (collision.gameObject.tag == "Bullet_2" | collision.gameObject.tag == "Bullet_1"))
         {
             collisiondelay = 0;
-            health = health - 1;
+            health = health - damage;
         }
     }
 }
